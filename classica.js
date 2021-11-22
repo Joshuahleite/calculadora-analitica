8d26ae8aae57b2879380e5686431925b420388ad
Joshuahleite <91674824+Joshuahleite@users.noreply.github.com>
2021-11-22 09:44:17 -0300

    js

---
+++ classica.js
@@ -0,0 +1,125 @@
+const display = document.getElementById("display")
+var numeroNovo = true;
+var operador;
+var numeroAnterior;
+
+
+function inserirNumero(numero) {
+atualizarDisplay(numero)
+}
+
+function atualizarDisplay(numero){
+if(numeroNovo == true){
+    display.value = numero
+    numeroNovo = false
+} else {
+    display.value += numero
+}
+}
+
+function backspace(){
+    display.value = display.value.slice(0, -1)
+}
+
+function selecionarOperaçao(operaçaoSelecionada){
+    if(!numeroNovo){
+        calcular()
+        numeroNovo = true
+        operador = operaçaoSelecionada
+        numeroAnterior = Number(display.value)
+
+    }
+}
+
+function operaçaoPendente() {
+    return operador !== undefined;
+}
+
+function calcular(){
+    if(operaçaoPendente()){
+        var numeroAtual = Number(display.value)
+        numeroNovo = true
+        if(operador =="+"){
+            atualizarDisplay(numeroAnterior + numeroAtual)
+        } else if(operador == "-"){
+            atualizarDisplay(numeroAnterior - numeroAtual)
+        } else if(operador == "*"){
+            atualizarDisplay(numeroAnterior * numeroAtual)
+        } else if(operador == "/"){
+            atualizarDisplay(numeroAnterior / numeroAtual)
+        }
+    }
+
+    }
+
+    function chamarIgual(){
+        calcular();
+        operador = undefined
+    }
+
+    // function resultado(){
+    //     calcular()
+    //     operador=undefined
+    //     return display.value
+    // }
+
+function limpar(){
+   display.value = "0"
+   numeroNovo = true
+   numeroAnterior = undefined;
+   operador = undefined
+}
+
+function inserirDecimal() {
+    // if(display.value.indexOf(",") == -1){
+    if(display.value.indexOf('.')== -1){
+        if(display.value.length >0){
+            atualizarDisplay('.');
+        } else{
+            atualizarDisplay('0.')
+        }
+        
+    }
+}
+
+function clicouTecla(event){
+
+    switch(event.key){
+        case '1': 
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '0':
+            inserirNumero(event.key)
+            break;
+            case 'Delete':
+            backspace();
+                    break;
+            case '+':
+            case '-':
+            case '/':
+            case '*':
+                selecionarOperaçao(event.key)
+                break;
+                case 'P':
+                    selecionarOperaçao('+');
+                case 'c':
+                limpar();
+                break;
+                case 'Enter':
+                case '=':
+                chamarIgual();
+                break;
+                case '.':
+                case ',':
+                inserirDecimal();
+                break;
+
+        
+    }
+}
